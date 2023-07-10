20b86d7a29609955b737dd548859bce0b1c46fc0
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2023-07-10 17:32:14 +0300

    carousel-slider and first steps of mobile-version

--- assests/js/carousel.js
+++ assests/js/carousel.js
@@ -7,22 +7,52 @@ const nextButton = document.getElementById("slide-arrow-next");
 const prevButton2 = document.getElementById("slide-arrow-prev-2");
 const nextButton2 = document.getElementById("slide-arrow-next-2");
 
+let indicator = 0;
+var element = document.getElementById("slide-1");
+
 nextButton.addEventListener("click", (event)=>{
-    const slideWidth = slide.clientWidth;
+        const slideWidth = slide.clientWidth;
         slidesContainer.scrollLeft += slideWidth;
+        if(indicator<4){
+          indicator+=1 ;
+          console.log(indicator);
+        }
+        element.classList.remove("fill-element");
+        var nextEl = element.nextSibling;
+        nextEl.classList.add("fill-element");
+        // if(indicator==0){
+        //   var element = document.getElementById("slide-1");
+        //   element.classList.add("fill-element");
+        // }
+        // else if(indicator=1){
+        //   var element = document.getElementById("slide-2");
+        //   element.classList.add("fill-element");
+        // }
 });
 
 prevButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft -= slideWidth;
+    if(indicator>0){
+      indicator-=1 ;
+      console.log(indicator);
+    }
   });
 
 nextButton2.addEventListener("click", (event)=>{
     const slideWidth = slide.clientWidth;
-        slidesContainer2.scrollLeft += slideWidth;
+    slidesContainer2.scrollLeft += slideWidth + 117;
+    if(indicator<4){
+      indicator+=1 ;
+      console.log(indicator);
+    } 
 });
 
 prevButton2.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer2.scrollLeft -= slideWidth;
+    if(indicator>0){
+      indicator-=1 ;
+      console.log(indicator);
+    }
   });
