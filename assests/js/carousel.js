875d25540f2aecaeb6890d05b8f7745af9ea64cd
bunnyt29 <87562845+bunnyt29@users.noreply.github.com>
2023-07-12 16:01:46 +0300

    mobile version without footer

--- assests/js/carousel.js
+++ assests/js/carousel.js
@@ -1,6 +1,7 @@
 const slidesContainer = document.getElementById("slides-container");
 const slidesContainer2 = document.getElementById("slides-container-2");
 const slide = document.querySelector(".slide");
+const slide2 = document.querySelector(".slide-2");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
@@ -13,43 +14,40 @@ var element = document.getElementById("slide-1");
 nextButton.addEventListener("click", (event)=>{
         const slideWidth = slide.clientWidth;
         slidesContainer.scrollLeft += slideWidth;
-        if(indicator<4){
-          indicator+=1 ;
-          console.log(indicator);
+        let circles = document.querySelectorAll(".slider-icon");
+        circles.forEach((circle)=>{
+          circle.classList.remove("active");
+        });
+        indicator++;
+        console.log(indicator, circles);
+        if(indicator>circles.length-1){
+          indicator=0;
         }
-        element.classList.remove("fill-element");
-        var nextEl = element.nextSibling;
-        nextEl.classList.add("fill-element");
-        // if(indicator==0){
-        //   var element = document.getElementById("slide-1");
-        //   element.classList.add("fill-element");
-        // }
-        // else if(indicator=1){
-        //   var element = document.getElementById("slide-2");
-        //   element.classList.add("fill-element");
-        // }
+        circles[indicator].classList.add("active");
 });
 
 prevButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft -= slideWidth;
-    if(indicator>0){
-      indicator-=1 ;
-      console.log(indicator);
+    let circles = document.querySelectorAll(".slider-icon");
+    circles.forEach((circle)=>{
+      circle.classList.remove("active");
+    });
+    indicator--;
+    console.log(indicator, circles);
+    if(indicator<0){
+      indicator=circles.length-1;
     }
+    circles[indicator].classList.add("active");
   });
 
 nextButton2.addEventListener("click", (event)=>{
-    const slideWidth = slide.clientWidth;
-    slidesContainer2.scrollLeft += slideWidth + 117;
-    if(indicator<4){
-      indicator+=1 ;
-      console.log(indicator);
-    } 
+    const slideWidth = slide2.clientWidth;
+    slidesContainer2.scrollLeft += slideWidth;
 });
 
 prevButton2.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
+    const slideWidth = slide2.clientWidth;
     slidesContainer2.scrollLeft -= slideWidth;
     if(indicator>0){
       indicator-=1 ;
